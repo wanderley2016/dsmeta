1ecfb674f17fc11b71d69f0dc030f4ca581a0344
wanderley <wanderleylandim@gmail.com>
2022-09-07 14:37:04 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NotificationButton from "./components/notificationbutton"
+
 function App() {
-return(
-  <h1>"Olá mundo!"</h1>
-) 
+  return (
+    <>
+      <h1>"Olá mundo!"</h1>
+      <NotificationButton />
+    </>
+  )
 }
+
 
 export default App
