0533b2278b887148dc4eeb6c7851b574cb23596a
wanderley <wanderleylandim@gmail.com>
2022-09-07 16:30:16 -0300

    Datepicker

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,6 @@
 
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from '../notificationbutton'
 import './styles.css'
 
@@ -8,10 +10,20 @@ function SalesCard() {
       <h2 className="dsmeta-sales-title">Vendas</h2>
       <div>
         <div className="dsmeta-form-control-container">
-          <input className="dsmeta-form-control" type="text" />
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
         <div className="dsmeta-form-control-container">
-          <input className="dsmeta-form-control" type="text" />
+          <DatePicker
+            selected={new Date()}
+            onChange={(date: Date) => { }}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
       </div>
 
@@ -38,7 +50,7 @@ function SalesCard() {
               <td>R$ 55300.00</td>
               <td>
                 <div className="dsmeta-red-btn-container">
-                  <NotificationButton/>
+                  <NotificationButton />
                 </div>
               </td>
             </tr>
@@ -51,7 +63,7 @@ function SalesCard() {
               <td>R$ 55300.00</td>
               <td>
                 <div className="dsmeta-red-btn-container">
-                <NotificationButton/>
+                  <NotificationButton />
                 </div>
               </td>
             </tr>
@@ -64,7 +76,7 @@ function SalesCard() {
               <td>R$ 55300.00</td>
               <td>
                 <div className="dsmeta-red-btn-container">
-                <NotificationButton/>
+                  <NotificationButton />
                 </div>
               </td>
             </tr>
