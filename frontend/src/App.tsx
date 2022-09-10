a51a1df7680b17faca2a10aec14bfb33d8b6ccff
wanderley <wanderleylandim@gmail.com>
2022-09-10 17:10:31 -0300

    Toast - componente para receber mensagem na tela

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
-
-
+import { ToastContainer } from 'react-toastify'; // para receber mensagem na tela  
+import 'react-toastify/dist/ReactToastify.css'; //  para receber mensagem na tela 
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
