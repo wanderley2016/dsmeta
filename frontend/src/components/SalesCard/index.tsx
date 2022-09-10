ceabd52243fdba788f20ab1874de1621b75dd318
wanderley <wanderleylandim@gmail.com>
2022-09-10 16:22:09 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -19,11 +19,14 @@ function SalesCard() {
 
   //Retorna os dados do banco de dados faz a requisição
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`)
+    const dmin = minDate.toISOString().slice(0, 10);
+    const dmax = maxDate.toISOString().slice(0, 10);
+
+    axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
       .then(response => {
         setSales(response.data.content)
       })
-  }, []);
+  }, [minDate, maxDate]);
 
   return (
     <div className="dsmeta-card">
